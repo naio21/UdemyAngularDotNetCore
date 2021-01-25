b4651cb5376b06df160e4d3e4ce19b12e492b7e4
naio21 <ivanbprado@gmail.com>
2021-01-25 20:02:11 -0300

    Implement EF Core Migrations and usage

--- ProAgil.WebAPI/Controllers/EventoController.cs
+++ ProAgil.WebAPI/Controllers/EventoController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
@@ -12,40 +11,29 @@ namespace ProAgil.WebAPI.Controllers
     public class EventoController : ControllerBase
     {
         private readonly ILogger<EventoController> _logger;
-        public EventoController(ILogger<EventoController> logger)
+        private readonly DataContext _context;
+
+        public EventoController(ILogger<EventoController> logger, DataContext context)
         {
-            _logger = logger;
+            this._logger = logger;
+            this._context = context;
         }
 
-        private Evento[] Eventos()
+        private List<Evento> Eventos()
         {
-            return new Evento[]
-            {
-                new Evento()
-                {
-                    EventoId = 1,
-                    Tema = "Angular e .NET Core",
-                    Lote = "1º Lote",
-                    QtdPessoas = 250,
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
-                },
-                new Evento()
-                {
-                    EventoId = 2,
-                    Tema = "Angular e Suas Novidades",
-                    Lote = "2º Lote",
-                    QtdPessoas = 350,
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy")
-                }
-            };
-         }
+            return _context.Eventos.ToList();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Evento>> Get()
+        {
+            return Eventos();
+        }
 
         [HttpGet("{id}")]
         public ActionResult<Evento> GetAction(int id)
         {
             return Eventos().FirstOrDefault(x => x.EventoId == id);
         }
-
-
     }
 }
---
+++ ProAgil.WebAPI/Migrations/20210125223533_Second.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.WebAPI.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImagemUrl",
+                table: "Eventos",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImagemUrl",
+                table: "Eventos");
+        }
+    }
+}
