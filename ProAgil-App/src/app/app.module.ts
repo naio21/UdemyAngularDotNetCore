1a09b84a05197db1b179beb3ba6bb2be77619f19
naio21 <ivanbprado@gmail.com>
2021-02-02 02:15:12 -0300

    Criação de entidades de domínio

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from "@angular/common/http";
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [		
+  declarations: [
     AppComponent,
       EventosComponent,
       NavComponent
@@ -16,7 +16,8 @@ import { NavComponent } from './nav/nav.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,19 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+// tslint:disable: variable-name
+// tslint:disable: typedef
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
-  styleUrls: ['./eventos.component.css']
+  styleUrls: ['./eventos.component.css'],
 })
 export class EventosComponent implements OnInit {
-
   Eventos: any = [];
+  EventosFiltrados: any = [];
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
-  
-  constructor(private http: HttpClient) { }
+
+  _filtroLista = '';
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.EventosFiltrados = this.filtroLista
+      ? this.filtrarEventos(this._filtroLista)
+      : this.Eventos;
+  }
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.getEventos();
@@ -24,10 +37,21 @@ export class EventosComponent implements OnInit {
   }
 
   getEventos() {
-    this.http.get('http://localhost:5000/evento').subscribe(response => {
-      this.Eventos = response;
-    }, error => {
-      console.log(error);
-    });
+    this.http.get('http://localhost:5000/evento').subscribe(
+      (response) => {
+        this.Eventos = response;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  filtrarEventos(filtro: string): any {
+    filtro = filtro.toLocaleLowerCase();
+    return this.Eventos.filter(
+      (evento: { tema: string }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtro) !== -1
+    );
   }
 }
